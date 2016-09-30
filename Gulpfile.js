de2e6d3a589e84b1f13247117ce8ed1f050f20ad
Mike Brewington <brewington@gmail.com>
2016-09-30 09:00:10 -0400

    style updates. js added

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,11 +3,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var connect = require('gulp-connect');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var del = require('del');
 
+var paths = {
+  // scripts: 'src/scripts/**/*'
+}
 var src = {
     scss: 'src/scss/**/*.scss',
-    // css:  'app/css',
-    html: 'src/*.html'
+    html: 'src/*.html',
+    scripts: 'src/scripts/**/*',
 };
 
 gulp.task('connect', function() {
@@ -17,8 +24,27 @@ gulp.task('connect', function() {
   });
 });
 
+gulp.task('clean', function() {
+  // You can use multiple globbing patterns as you would with `gulp.src`
+  return del(['public']);
+});
+
+gulp.task('scripts', ['clean'], function() {
+  // Minify and copy all JavaScript (except vendor scripts)
+  // with sourcemaps all the way down
+  return gulp.src(src.scripts)
+    .pipe(sourcemaps.init())
+      // .pipe(coffee())
+      .pipe(uglify())
+      .pipe(concat('all.min.js'))
+    // .pipe(sourcemaps.write())
+    .pipe(gulp.dest('public/js'));
+});
+
 gulp.task('sass', function () {
   return gulp.src('./src/sass/**/*.scss')
+  // return gulp.src(src.scss)
+    .pipe(connect.reload())
     .pipe(sass.sync().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'));
 });
@@ -32,6 +58,7 @@ gulp.task('html', function () {
 gulp.task('watch', function () {
     gulp.watch('src/sass/**/*.scss', ['sass']);
     gulp.watch('src/**/*.html', ['html']);
+    gulp.watch('src/scripts/**/*', ['scripts']);
 });
 
 gulp.task('default', ['connect', 'watch']);
